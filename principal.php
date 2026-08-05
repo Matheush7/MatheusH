f7e2158fb36177a0443eb9029e649837aee38d00
Matheush7 <matheus44.henrique@gmail.com>
2026-08-05 10:36:16 -0300

    Add files via upload

---
+++ principal.php
@@ -0,0 +1,15 @@
+<?php
+/******************************************************************************
+
+$Matheus
+$nota1 = 
+$nota2 = 
+$nota3 = 
+*******************************************************************************/
+
+echo "Hello Matheus\n";
+print("nota1 = 5.5\n");
+print("nota2 = 6.5\n");
+print("nota3 = 7.5\n");
+echo("Qual é a media dessas tres notas?\n");
+readline("A média das tres notas é 6.5\n");
